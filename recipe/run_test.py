30bba231419958676c18b525c3b58b41ca788596
Peter Williams <peter@newton.cx>
2019-09-20 17:16:55 -0400

    recipe/run_test.py: get diagnostics if module import fails

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -5,6 +5,12 @@ from jupyterlab.commands import get_app_info
 from notebook.nbextensions import validate_nbextension
 from notebook.serverextensions import validate_serverextension
 
+# Try the import. If it doesn't work, the serverextension validation below
+# will crash without showing us what the problem is. Note that this script
+# runs before the generic Conda import test, so if the import is broken that
+# test won't run either.
+import pywwt
+
 # If there's a problem and we don't provide this, the validate function crashes :-(
 logger = logging.getLogger('')
 
