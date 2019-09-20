fe9da98296dee8eb44981586bb6ea983550c20bf
Peter Williams <peter@newton.cx>
2019-09-20 15:13:07 -0400

    recipe/run_test.py: fix error reporting if server extension breaks
    Cf pull #187 to pywwt where the same issue came up.

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,11 +1,14 @@
+import logging
 import sys
 
 from jupyterlab.commands import get_app_info
 from notebook.nbextensions import validate_nbextension
 from notebook.serverextensions import validate_serverextension
 
+# If there's a problem and we don't provide this, the validate function crashes :-(
+logger = logging.getLogger('')
 
-if validate_nbextension('pywwt/extension') != []:
+if validate_nbextension('pywwt/extension', logger=logger) != []:
     print("Issue detected with nbextension")
     sys.exit(1)
 print("nbextension OK")
@@ -17,7 +20,7 @@ if 'pywwt' not in info['extensions'] or 'pywwt' in info['disabled']:
     sys.exit(1)
 print("labextension OK")
 
-if validate_serverextension('pywwt') != []:
+if validate_serverextension('pywwt', logger=logger) != []:
     print("Issue detected with serverextension")
     sys.exit(1)
 print("serverextension OK")
